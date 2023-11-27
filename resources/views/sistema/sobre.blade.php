4dd1e5e49edac5aaf50eb7de50c0089071e02be1
JAMB123 <brenomourasandrade@gmail.com>
2023-11-27 17:41:59 -0300

    Tudo certo

--- resources/views/sistema/sobre.blade.php
+++ resources/views/sistema/sobre.blade.php
@@ -2,20 +2,16 @@
 @section('title','JAMB')
 @section('content')
 
-
-    <div class="row">
-        <div class="col-sm-7">
-            <img src="{{asset('storage/imagens/sobreadoc.png')}}" style="width: 700px" />
-        </div>
-        <div class="col-sm-5" id="sobre">
+        <div class="col-md-12" id="sobre">
             <div class="container">
                 <p class="h4 text-center" style="color: #2B1998; margin-top: 75px; font-size: 70px"><b>Sobre a causa</b></p>
-                <p class="h1 text-start" style=" font-size: 40px; margin-left: 30px">
+                <p class="h1 text-start" style=" font-size: 25px; margin-left: 20px">o cepetz é um projeto de extensão do cefet-mg, formado por alunos e funcionários voluntários, que surgiu, inicialmente, no campus x, que está localizado na cidade de curvelo, com o objetivo de dar assistência aos animais que eram abandonados no campus e sensibilizar e conscientizar a comunidade em relação ao abandono de animais, prezando sempre pela ética e responsabilidade social. vendo essa ideia de projeto maravilhosa do campus x, o campus viii, que está localizado na cidade de varginha, teve interesse em expandir esse projeto pra cá, apresentando a ideia aos alunos e formando um grupo de voluntários com o objetivo de, além de conscientizar a população, ajudar e apoiar o sítio maria josé semionato, que abriga cerca de 170 cachorros e alguns gatos. visando ajudar o sítio maria josé semionato, os voluntários do projeto abriram um bazar para a arrecadação de fundos que vão diretamente para o sítio, para a compra de ração e itens para que os animais estejam sempre alimentados e bem cuidados. os voluntários também fazem a divulgação para a arrecadação também de doações, tanto em pacotes de ração, quanto em dinheiro, que vai direto para a conta do sítio.
+
+
             
-O CEPETZ É UM PROJETO DE EXTENSÃO DO CEFET- MG, FORMADO POR ALUNOS E FUNCIONÁRIOS VOLUNTÁRIOS, QUE SURGIU, INICIALMENTE, NO CAMPUS X, QUE ESTÁ LOCALIZADO NA CIDADE DE CURVELO, COM O OBJETIVO DE DAR ASSISTÊNCIA AOS ANIMAIS QUE ERAM ABANDONADOS NO CAMPUS E SENSIBILIZAR E CONSCIENTIZAR A COMUNIDADE EM RELAÇÃO AO ABANDONO DE ANIMAIS, PREZANDO SEMPRE PELA ÉTICA E RESPONSABILIDADE SOCIAL.
-</p>
 
             </div>
+            <img src="{{asset('storage/images/fotocepetz.jpg')}}" style="width: 650px; margin-left: 600px" />
         </div>
 
 
